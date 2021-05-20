06615e3d5e03b8874b620735c7350be2131e6483
Julian Hecker <julianheckerdev@gmail.com>
2021-05-20 07:35:40 -0400

    fix context

--- client/src/context/UserContext.tsx
+++ client/src/context/UserContext.tsx
@@ -2,6 +2,12 @@ import React, { createContext, useEffect, useState } from 'react';
 
 // https://kentcdodds.com/blog/how-to-use-react-context-effectively
 
+// Using context with localstorage
+// https://gist.github.com/jimode/c1d2d4c1ab33ba1b7be8be8c50d64555
+
+// How to update context from child
+// https://dev.to/oieduardorabelo/react-hooks-how-to-create-and-update-contextprovider-1f68
+
 export const UserContext = createContext<any>({});
 
 const UserContextProvider: React.FC = ({ children }: any) => {
@@ -15,7 +21,7 @@ const UserContextProvider: React.FC = ({ children }: any) => {
   }, []);
 
   return (
-    <UserContext.Provider value={user}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );

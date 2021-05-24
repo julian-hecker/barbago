4f100be17f87f3b087d09e617eb5aec41d5c2283
Julian Hecker <julianheckerdev@gmail.com>
2021-05-24 03:42:31 -0400

    Add messaging section

--- client/src/pages/Messages/Messages.tsx
+++ client/src/pages/Messages/Messages.tsx
@@ -20,16 +20,24 @@ const Messages: React.FC = () => {
   useEffect(() => {
     setConversations([
       {
-        name: 'hello',
-        lastMessage: 'oh hey there',
-        lastMessageDate: new Date(),
+        name: 'Clientus Ignorabamus',
+        lastMessage:
+          "oh hey there what's up! I totally missed our appt last week, totally sorry bout that! :P",
+        lastMessageDate: new Date(2021, 4, 20, 23, 30),
         convoAvatar:
           'https://lh3.googleusercontent.com/ogw/ADGmqu_frRjTU7Mt_WO4GLdRTWkM3NST1w6Re0xp-cof=s32-c-mo',
       },
       {
-        name: 'goodbye',
-        lastMessage: 'oh hey there',
-        lastMessageDate: new Date(),
+        name: 'Fallacius Maxillianus',
+        lastMessage: 'Any appointments for last week?',
+        lastMessageDate: new Date(2021, 4, 10, 23, 30),
+        convoAvatar:
+          'https://lh3.googleusercontent.com/ogw/ADGmqu_frRjTU7Mt_WO4GLdRTWkM3NST1w6Re0xp-cof=s32-c-mo',
+      },
+      {
+        name: 'Fraternus Maxillianus',
+        lastMessage: 'boku no imouto taberu gohan',
+        lastMessageDate: new Date(2021, 4, 30, 23, 30),
         convoAvatar:
           'https://lh3.googleusercontent.com/ogw/ADGmqu_frRjTU7Mt_WO4GLdRTWkM3NST1w6Re0xp-cof=s32-c-mo',
       },
@@ -47,20 +55,27 @@ const Messages: React.FC = () => {
         <IonNav></IonNav>
         <IonList>
           {conversations.map(
-            ({ name, lastMessage, lastMessageDate, convoAvatar }) => (
-              <IonItem>
-                <IonAvatar>
-                  <IonImg src={convoAvatar} alt={name} />
-                </IonAvatar>
-                <div style={{ marginLeft: '1rem' }}>
-                  <h3>{name}</h3>
-                  <p>{lastMessage}</p>
-                  <p>
-                    <small>{lastMessageDate.toString()}</small>
-                  </p>
-                </div>
-              </IonItem>
-            ),
+            ({ name, lastMessage, lastMessageDate, convoAvatar }) => {
+              const datediff =
+                (
+                  (new Date().getTime() - lastMessageDate.getTime()) /
+                  3600000
+                ).toFixed(1) + ' hours ago';
+              return (
+                <IonItem>
+                  <IonAvatar style={{ minWidth: '40px' }}>
+                    <IonImg src={convoAvatar} alt={name} />
+                  </IonAvatar>
+                  <div style={{ marginLeft: '1rem' }}>
+                    <h3>{name}</h3>
+                    <p>{lastMessage.slice(0, 50)}</p>
+                    <p>
+                      <small>{datediff}</small>
+                    </p>
+                  </div>
+                </IonItem>
+              );
+            },
           )}
         </IonList>
       </IonContent>

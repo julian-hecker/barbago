dc095f04ec501b6b80f80cac6be8e5335837c4cd
Julian Hecker <julianheckerdev@gmail.com>
2021-05-25 03:09:25 -0400

    add map markers

--- client/src/pages/Search/SearchMap/SearchMap.tsx
+++ client/src/pages/Search/SearchMap/SearchMap.tsx
@@ -6,31 +6,42 @@ import {
   Marker,
   Popup,
 } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import { ISearchResult } from '../SearchResult';
 
-const SearchMap: React.FC<ISearchMapProps> = ({ results }) => {
-  // const map = useMap();
+import icon from '../../../assets/marker-icon.png';
 
+const pinIcon = L.icon({
+  iconUrl: icon,
+  iconAnchor: [12.5, 50],
+  iconSize: [25, 50],
+  popupAnchor: [0, -25],
+});
+
+const SearchMap: React.FC<ISearchMapProps> = ({ results }) => {
   return (
     <MapContainer
       center={[41.0, -73.83]}
       zoom={10}
-      minZoom={5}
+      minZoom={4}
+      scrollWheelZoom={true}
+      dragging={true}
+      maxBoundsViscosity={1}
       maxBounds={[
         [-90, -180],
         [90, 180],
       ]}
-      maxBoundsViscosity={1}
-      scrollWheelZoom={true}
       style={{ height: '100%', width: '100%' }}
     >
-      <TileLayer url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png" />
-      {/* attribution='&copy; <a href="http://maps.stamen.com/">Stamen Maps</a>' */}
+      <TileLayer
+        url="https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.png"
+        attribution="&copy; Stamen Maps"
+      />
       {results.map(({ name, position }, index) =>
         position ? (
-          <Marker position={position} key={index}>
+          <Marker position={position} key={index} icon={pinIcon}>
             <Popup>
               <h3>{name}</h3>
               <p>{position}</p>

815bf71b308520422939d1c5947f36d865d9315e
Julian Hecker <julianheckerdev@gmail.com>
2021-06-10 03:53:09 -0400

    Create login and signup pages

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,17 +1,110 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import {
+  logoFacebook,
+  logoGoogle,
+  lockClosed,
+  mail,
+} from 'ionicons/icons';
 
-import { UserContext } from '../../context/UserContext';
+import icon from '../../assets/icon.png';
 
 const Login: React.FC = () => {
-  const { setUser } = useContext(UserContext);
-
   return (
-    <div>
-      Login
-      <button onClick={() => setUser({ user: 'someone' })}>
-        aaaaaaaa
-      </button>
-    </div>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonImg
+              src={icon}
+              style={{
+                maxWidth: '50%',
+                width: '200px',
+                margin: 'auto',
+              }}
+            />
+            {/* Welcome Back! */}
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              <IonItem>
+                <IonLabel position="floating">Email Address</IonLabel>
+                <IonInput />
+                <IonIcon
+                  icon={mail}
+                  slot="start"
+                  className="ion-align-self-center"
+                  color="primary"
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">Password</IonLabel>
+                <IonInput />
+                <IonIcon
+                  icon={lockClosed}
+                  slot="start"
+                  className="ion-align-self-center"
+                  color="primary"
+                />
+              </IonItem>
+            </IonList>
+            <IonButton expand="block">Log In</IonButton>
+            <p
+              style={{
+                margin: '0.5rem 0',
+                color: 'var(--ion-color-step-650)',
+                textAlign: 'center',
+              }}
+            >
+              — Or —
+            </p>
+            <IonButton
+              expand="block"
+              color="light"
+              href="/auth/google"
+              target="_blank"
+            >
+              <IonIcon slot="start" icon={logoGoogle} />
+              Log in with Google
+            </IonButton>
+            <IonButton
+              expand="block"
+              color="light"
+              href="/auth/facebook"
+              target="_blank"
+            >
+              <IonIcon slot="start" icon={logoFacebook} />
+              Log in with Facebook
+            </IonButton>
+            <p style={{ marginTop: '1rem', textAlign: 'center' }}>
+              Don't have an account? <Link to="/signup">Sign up</Link>
+            </p>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
   );
 };
 
--- client/src/pages/Signup/Signup.tsx
+++ client/src/pages/Signup/Signup.tsx
@@ -1,7 +1,131 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
+import {
+  logoFacebook,
+  logoGoogle,
+  lockClosed,
+  mail,
+  person,
+} from 'ionicons/icons';
 
+
+// Problem: Can't test on Mobile; should open a new tab
 const Signup: React.FC = () => {
-  return <div>Signup</div>;
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Signup</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Welcome to Barbago!</IonCardTitle>
+            <IonCardSubtitle>
+              You can sign up using your email address or a provider
+              like Google or Facebook.
+            </IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              <IonItem>
+                <IonLabel position="floating">Name</IonLabel>
+                <IonInput />
+                <IonIcon
+                  icon={person}
+                  slot="start"
+                  className="ion-align-self-center"
+                  color="primary"
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">Email Address</IonLabel>
+                <IonInput />
+                <IonIcon
+                  icon={mail}
+                  slot="start"
+                  className="ion-align-self-center"
+                  color="primary"
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">Password</IonLabel>
+                <IonInput />
+                <IonIcon
+                  icon={lockClosed}
+                  slot="start"
+                  className="ion-align-self-center"
+                  color="primary"
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position="floating">
+                  Confirm Password
+                </IonLabel>
+                <IonInput />
+                <IonIcon
+                  icon={lockClosed}
+                  slot="start"
+                  className="ion-align-self-center"
+                  color="primary"
+                />
+              </IonItem>
+            </IonList>
+            <IonButton expand="block">Signup</IonButton>
+            <p
+              style={{
+                margin: '0.5rem 0',
+                color: 'var(--ion-color-step-650)',
+                textAlign: 'center',
+              }}
+            >
+              — Or —
+            </p>
+            <IonButton
+              expand="block"
+              color="light"
+              href="/auth/google"
+              target="_blank"
+            >
+              <IonIcon slot="start" icon={logoGoogle} />
+              Signup with Google
+            </IonButton>
+            <IonButton
+              expand="block"
+              color="light"
+              href="/auth/facebook"
+              target="_blank"
+            >
+              <IonIcon slot="start" icon={logoFacebook} />
+              Signup with Facebook
+            </IonButton>
+            <p style={{ marginTop: '1rem', textAlign: 'center' }}>
+              Already have an account? <Link to="/login">Log in</Link>
+            </p>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 export default Signup;

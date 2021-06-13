6a47ae7192f3df050ea8b63bf58bf7fd1699babe
Julian Hecker <julianheckerdev@gmail.com>
2021-06-13 14:21:42 -0400

    extract routes into an object

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,10 +10,9 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { chatbubbles, home, search, settings } from 'ionicons/icons';
-import { Switch, Route, Redirect } from 'react-router-dom';
-// import axios from 'axios';
+import { Switch, Redirect } from 'react-router-dom';
 
-import PrivateRoute from './components/PrivateRoute';
+import ConditionalRoute from './components/ConditionalRoute';
 import { UserContext } from './context/UserContext';
 
 import Welcome from './pages/Welcome/Welcome';
@@ -23,7 +22,10 @@ import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import Settings from './pages/Settings/Settings';
 import Messages from './pages/Messages/Messages';
-// Account? Profile? Edit Profile? Services? Schedule? Map?
+// Account? Profile? Edit Profile? Services? Schedule?
+
+// To do: Add application-level notification area
+// Decide on using firebase or node-express for backend
 
 const App: React.FC = () => {
   const { user } = useContext(UserContext);
@@ -32,6 +34,62 @@ const App: React.FC = () => {
     console.log(user);
   }, [user]);
 
+  const routes = [
+    {
+      path: '/:tab(home)',
+      component: Home,
+    },
+    {
+      path: '/:tab(search)',
+      component: Search,
+    },
+    {
+      path: '/:tab(messages)',
+      component: Messages,
+      condition: user.user,
+      redirect: '/signup',
+    },
+    {
+      path: '/:tab(settings)',
+      component: Settings,
+    },
+    {
+      path: '/welcome',
+      component: Welcome,
+    },
+    {
+      path: '/login',
+      component: Login,
+    },
+    {
+      path: '/signup',
+      component: Signup,
+    },
+  ];
+
+  const tabs = [
+    {
+      text: 'Home',
+      path: '/home',
+      icon: home,
+    },
+    {
+      text: 'Search',
+      path: '/search',
+      icon: search,
+    },
+    {
+      text: 'Messages',
+      path: '/messages',
+      icon: chatbubbles,
+    },
+    {
+      text: 'Settings',
+      path: '/settings',
+      icon: settings,
+    },
+  ];
+
   return (
     <IonApp>
       <IonReactRouter>
@@ -39,37 +97,27 @@ const App: React.FC = () => {
           <IonRouterOutlet>
             <Switch>
               <Redirect exact path="/" to="/welcome" />
-
-              <Route path="/:tab(home)" component={Home} />
-              <Route path="/:tab(search)" component={Search} />
-              <PrivateRoute
-                path="/:tab(messages)"
-                component={Messages}
-              />
-              <Route path="/:tab(settings)" component={Settings} />
-
-              <Route path="/welcome" component={Welcome} />
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={Signup} />
+              {routes.map(
+                ({ path, component, condition, redirect }) => {
+                  return (
+                    <ConditionalRoute
+                      path={path}
+                      component={component}
+                      condition={condition}
+                      redirect={redirect}
+                    />
+                  );
+                },
+              )}
             </Switch>
           </IonRouterOutlet>
           <IonTabBar slot="bottom">
-            <IonTabButton tab="home" href="/home">
-              <IonIcon icon={home} />
-              <IonLabel>Home</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="search" href="/search">
-              <IonIcon icon={search} />
-              <IonLabel>Search</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="messages" href="/messages">
-              <IonIcon icon={chatbubbles} />
-              <IonLabel>Chat</IonLabel>
-            </IonTabButton>
-            <IonTabButton tab="settings" href="/settings">
-              <IonIcon icon={settings} />
-              <IonLabel>Settings</IonLabel>
-            </IonTabButton>
+            {tabs.map(({ text, path, icon }) => (
+              <IonTabButton tab={text} href={path}>
+                <IonIcon icon={icon} />
+                <IonLabel>{text}</IonLabel>
+              </IonTabButton>
+            ))}
           </IonTabBar>
         </IonTabs>
       </IonReactRouter>
@@ -78,6 +126,3 @@ const App: React.FC = () => {
 };
 
 export default App;
-
-
-// Extract Routes into an object

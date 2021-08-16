37e7e4bdc604ab1ed09c08d42c02eeeea77e7418
Julian Hecker <julianheckerdev@gmail.com>
2021-08-16 01:29:45 -0400

    typo

--- client/src/screens/other/PrivacyScreen.tsx
+++ client/src/screens/other/PrivacyScreen.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { TextScreen, Text, Header } from '../../components';
+import { TextScreen, Paragraph, Header } from '../../components';
 
 const PrivacyScreen = () => {
   return (
     <TextScreen>
       <Header>Barbago Privacy Policy</Header>
-      <Text></Text>
+      <Paragraph>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
+        omnis, eligendi consequatur harum suscipit architecto ullam
+        aut et quaerat dolore.
+      </Paragraph>
     </TextScreen>
   );
 };
